e547cb9d81f0c040cd7427d0b2982eefa2f481c7
soybabydani-ops <soybabydani@gmail.com>
2026-08-03 13:40:11 -0300

    Add files via upload

--- assets/app.js
+++ assets/app.js
@@ -39,6 +39,7 @@
   var partes = location.hostname.split(".");
   var sub = (partes[0] || "www").toLowerCase();
   if (sub === "www" || partes.length <= 2 || sub === "localhost") sub = "www";
+  if (sub === "gramado" && (location.pathname === "/" || location.pathname === "/index.html")) { location.replace("/gramado-home.html"); }
   var par = CIDADES[sub] || CIDADES["www"];
   var CIDADE = par[0], UF = par[1], CIDUP = CIDADE.toUpperCase();
   window.CIDADE = { nome: CIDADE, uf: UF };
